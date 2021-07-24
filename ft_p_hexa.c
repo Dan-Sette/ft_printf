48f1e054385bc407a1eff8b666b36df21eb2134b
Dan-Sette <daniel.sette@hotmail>
2021-07-24 19:39:33 -0300

    Fixed function name

--- ft_p_hexa.c
+++ ft_p_hexa.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_hexa.c                                    :+:      :+:    :+:   */
+/*   ft_p_hexa.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dalves-s <dalves-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 21:26:46 by dalves-s          #+#    #+#             */
-/*   Updated: 2021/07/24 18:37:05 by dalves-s         ###   ########.fr       */
+/*   Updated: 2021/07/24 19:37:23 by dalves-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_print_hexa(unsigned long long int num, char *base)
+void	ft_p_hexa(unsigned long long int num, char *base)
 {
 	int		len_base;
 
 	len_base = ft_strlen(base);
 	if (num / len_base > 0)
-		ft_print_hexa(num / len_base, base);
+		ft_p_hexa(num / len_base, base);
 	ft_putchar_fd(base[num % len_base], 1);
 }
 
